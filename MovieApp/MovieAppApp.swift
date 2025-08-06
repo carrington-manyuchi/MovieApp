8994cba503057d0973ad888183fab3433391e33c
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-08-06 11:08:30 +0200

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Manyuchi, Carrington C on 2025/08/06.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
